c05935a214fca1976c83c57c1a93038964f6062c
Thuong-Tuan Tran <tuantran01@maisonrmi.com>
2025-07-07 09:17:32 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <svg xmlns="http:/

--- src/components/maison-logo.tsx
+++ src/components/maison-logo.tsx
@@ -14,12 +14,14 @@ export function MaisonLogo(props: SVGProps<SVGSVGElement>) {
         viewBox="0 0 24 24"
         fill="none"
         stroke="currentColor"
-        strokeWidth="2.5"
+        strokeWidth="2"
         strokeLinecap="round"
         strokeLinejoin="round"
         className="h-[55%] w-[55%]"
       >
-        <path d="M4 20V4l8 8 8-8v16" />
+        <path d="M6 2 3 6v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V6l-3-4Z" />
+        <path d="M3 6h18" />
+        <path d="M16 10a4 4 0 0 1-8 0" />
       </svg>
     </div>
   );
