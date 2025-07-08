e9a337fde217a7270349a2997d0f170a5774bb2e
Thuong-Tuan Tran <tuantran01@maisonrmi.com>
2025-07-08 03:16:08 +0000

    Make the following changes:
    ,
    
    Find this element:
    
    Relevant files:

--- src/components/brand-logo-placeholder.tsx
+++ src/components/brand-logo-placeholder.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 export function BrandLogoPlaceholder({ className }: { className?: string }) {
   return (
     <div className={cn("flex justify-center", className)}>
-      <div className="relative h-16 w-40">
+      <div className="relative h-20 w-60">
         <Image
           src="https://th.mlb-korea.com/cdn/shop/files/Logo_New-02.png?v=1684485771&width=309"
           alt="Brand Logo"
